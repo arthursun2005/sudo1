cac6fc2ce5173c3dd7c3260ae4cdcec4ddbb1a4f
Arthur Sun <arthur@HARRY-MAC.local>
2018-01-26 19:12:56 +1100

    test1

--- Sudoku.h
+++ Sudoku.h
@@ -1,3 +1,4 @@
+#include "math2.h"
 class Sudoku
 {
 public:
@@ -11,34 +12,52 @@ public:
 	void solve();
 private:
 };
-Sudoku::Sudoku(int c[]){
-	for(int i=0;i<81;++i){
-		cells[i] = c[i];
-	}
-}
-Sudoku::~Sudoku(){
-	//delete [] cells;
-}
-int Sudoku::get(int x, int y){
-	return cells[x+y*9];
-}
-void Sudoku::change(int x, int y, int n){
-	cells[x+y*9] = n;
-}
+Sudoku::Sudoku(int c[]){for(int i=0;i<81;++i){cells[i] = c[i];}}
+Sudoku::~Sudoku(){}
+int Sudoku::get(int x, int y){return cells[x+y*9];}
+void Sudoku::change(int x, int y, int n){cells[x+y*9] = n;}
 int Sudoku::check(){
-	int h1[9] = {0,0,0,0,0,0,0,0,0};
 	for(int x=0;x<9;++x){
+		int h1[9] = {0,0,0,0,0,0,0,0,0};
 		for(int y=0;y<9;++y){
+			int n = get(x,y);
+			if(n==0) continue;
+			if(h1[n-1]!=0){return false;}
+			h1[n-1] = n;
 		}
 	}
-}
-int Sudoku::done(){
-	for(int x=0;x<9;++x){
-		for(int y=0;y<9;++y){
-			if(get(x,y) == 0){
-				return false;
+	for(int y=0;y<9;++y){
+		int h1[9] = {0,0,0,0,0,0,0,0,0};
+		for(int x=0;x<9;++x){
+			int n = get(x,y);
+			if(n==0) continue;
+			if(h1[n-1]!=0){return false;}
+			if(n!=0){h1[n-1] = n;}
+		}
+	}
+	for(int nb=1;nb<=9;++nb){
+		int h1[9] = {0,0,0,0,0,0,0,0,0};
+		int nx = (nb-1)%3+1, ny = floor((nb-1)/3)+1;
+		for(int x=1;x<=3;++x){
+			for(int y=1;y<=3;++y){
+				int n = get((nx-1)*3+x,(ny-1)*3+y);
+				if(n==0) continue;
+				if(h1[n-1]!=0){return false;}
+				h1[n-1] = n;
 			}
 		}
 	}
 	return true;
+}
+int Sudoku::done(){
+	check();
+	for(int x=0;x<9;++x){
+		for(int y=0;y<9;++y){
+			if(get(x,y) == 0){return false;}
+		}
+	}
+	return true;
+}
+void Sudoku::solve(){
+	for(int x=1)
 }
---
+++ test1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "math2.h"
+using namespace std;
+int F(int n){
+	int num;
+	return num;
+}
+int main(void)
+{
+	int n = F(3);
+	cout << n << endl;
+	return 0;
+}
