032a6fb1a6d89121968af8820b1784dea9e49c30
Arthur Sun <arthur@HARRY-MAC.local>
2018-01-28 15:11:38 +1100

    update

--- Sudoku.h
+++ Sudoku.h
@@ -9,13 +9,23 @@ public:
 	void change(int x, int y, int n);
 	int done();
 	int check();
-	void solve();
+	int solve();
 private:
 };
-Sudoku::Sudoku(int c[]){for(int i=0;i<81;++i){cells[i] = c[i];}}
-Sudoku::~Sudoku(){}
-int Sudoku::get(int x, int y){return cells[x+y*9];}
-void Sudoku::change(int x, int y, int n){cells[x+y*9] = n;}
+Sudoku::Sudoku(int c[]){
+	for(int i=0;i<81;++i){
+		cells[i] = c[i];
+	}
+}
+Sudoku::~Sudoku(){
+	// delete memory
+}
+int Sudoku::get(int x, int y){
+	return cells[x+y*9];
+}
+void Sudoku::change(int x, int y, int n){
+	cells[x+y*9] = n;
+}
 int Sudoku::check(){
 	for(int x=0;x<9;++x){
 		int h1[9] = {0,0,0,0,0,0,0,0,0};
@@ -32,15 +42,15 @@ int Sudoku::check(){
 			int n = get(x,y);
 			if(n==0) continue;
 			if(h1[n-1]!=0){return false;}
-			if(n!=0){h1[n-1] = n;}
+			h1[n-1] = n;
 		}
 	}
-	for(int nb=1;nb<=9;++nb){
+	for(int nb=0;nb<9;++nb){
 		int h1[9] = {0,0,0,0,0,0,0,0,0};
-		int nx = (nb-1)%3+1, ny = floor((nb-1)/3)+1;
-		for(int x=1;x<=3;++x){
-			for(int y=1;y<=3;++y){
-				int n = get((nx-1)*3+x,(ny-1)*3+y);
+		int nx = nb%3, ny = floor(nb/3);
+		for(int x=0;x<3;++x){
+			for(int y=0;y<3;++y){
+				int n = get(nx*3+x,ny*3+y);
 				if(n==0) continue;
 				if(h1[n-1]!=0){return false;}
 				h1[n-1] = n;
@@ -50,14 +60,24 @@ int Sudoku::check(){
 	return true;
 }
 int Sudoku::done(){
-	check();
 	for(int x=0;x<9;++x){
-		for(int y=0;y<9;++y){
-			if(get(x,y) == 0){return false;}
-		}
+		for(int y=0;y<9;++y){if(get(x,y) == 0){return false;}}
 	}
 	return true;
 }
-void Sudoku::solve(){
-	for(int x=1)
+int Sudoku::solve(){
+	for(int i=0;i<81;++i){
+		if(cells[i] == 0){
+			for(int n=1;n<=9;++n){
+				cells[i] = n;
+				if(check()){
+					if(solve()){
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+	}
+	return true;
 }
--- sudo2.cpp
+++ sudo2.cpp
@@ -2,8 +2,32 @@
 #include "sudos.h"
 #include "Sudoku.h"
 using namespace std;
+void print(Sudoku s){
+	cout << endl << endl;
+	for(int y=0;y<9;++y){
+		cout << "|";
+		for(int x=0;x<9;++x){
+			int n = s.get(x,y);
+			if(n!=0){
+				cout << s.get(x,y);
+			}else{
+				cout << " ";
+			}
+			cout << "|";
+		}
+		cout << endl;
+	}
+	cout << endl << endl;
+}
 int main(int argc, char const *argv[])
 {
 	Sudoku s1(sudo1);
+	print(s1);
+	if(!s1.check()){
+		cout << "Error in the Sudoku" << endl;
+		return 0;
+	}
+	if(!s1.done()) s1.solve();
+	print(s1);
 	return 0;
 }
